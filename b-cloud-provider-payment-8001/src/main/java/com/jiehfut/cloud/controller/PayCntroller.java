3e71496eea74bdff0be1bf2615ae2356ea440264
JieCHERY <9594523wrj@gmail.com>
2025-01-16 15:44:33 +0800

    springcloud

--- b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/controller/PayCntroller.java
+++ b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/controller/PayCntroller.java
@@ -3,6 +3,7 @@ package com.jiehfut.cloud.controller;
 
 import com.jiehfut.cloud.entities.Pay;
 import com.jiehfut.cloud.entities.PayDTO;
+import com.jiehfut.cloud.resp.ResultData;
 import com.jiehfut.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -34,40 +35,42 @@ public class PayCntroller {
      */
     @PostMapping("/pay/add")
     @Operation(summary = "新增", description = "新增一条流水记录，JSON 串参数")
-    public String addPay(@RequestBody Pay pay) {
+    public ResultData<String> addPay(@RequestBody Pay pay) {
         System.out.println(pay.toString());
 
         int add = payService.add(pay);
-        return add == 1 ? "成功插入" : "插入失败";
+        return add == 1 ? ResultData.success("插入成功") : ResultData.fail("-1", "插入失败");
     }
 
     @DeleteMapping("/pay/del/{id}")
     @Operation(summary = "删除", description = "根据主键 ID 删除流水记录")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    public ResultData<String> deletePay(@PathVariable("id") Integer id) {
+        int delete = payService.delete(id);
+        return delete == 1 ? ResultData.success("删除成功") : ResultData.fail("-1", "删除失败");
     }
 
     @PutMapping("/pay/update")
     @Operation(summary = "更新", description = "更新流水记录")
-    public String updatePay(@RequestBody PayDTO payDTO) {
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         // BeanUtils 进行对拷
         BeanUtils.copyProperties(payDTO, pay);
         int update = payService.update(pay);
-        return update == 1 ? "更新成功" : "更新失败";
+        return update == 1 ? ResultData.success("更新成功") : ResultData.fail("-1", "更新失败");
     }
 
     @GetMapping("/pay/get/{id}")
     @Operation(summary = "根据主键 ID 来获取一条流水记录")
-    public Pay getPayById(@PathVariable("id") Integer id) {
+    public ResultData<Pay> getPayById(@PathVariable("id") Integer id) {
         Pay pay = payService.findById(id);
-        return pay;
+        return ResultData.success(pay);
     }
 
     @GetMapping("/pay/get")
     @Operation(summary = "获取全部流水", description = "获取全部流水记录")
-    public List<Pay> getPayList() {
-        return payService.findAll();
+    public ResultData<List<Pay>> getPayList() {
+        List<Pay> allPays = payService.findAll();
+        return ResultData.success(allPays);
     }
 
 }
--- b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/resp/ResultData.java
+++ b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/resp/ResultData.java
@@ -9,7 +9,8 @@ import lombok.experimental.Accessors;
  * @param <T>
  */
 @Data
-@Accessors(chain = true)
+@Accessors(chain = true) // 开启链式编程：设置chain=true时，setter方法返回的是this（也就是对象自己）
+                         // 代替了默认的返回值void，直接链式操作对象（如 user.setName("aaa").setAge(18).set().set()）
 public class ResultData<T> {
 
     private String code;/** 结果状态 ,具体状态码参见枚举类 ReturnCodeEnum.java*/
@@ -17,11 +18,17 @@ public class ResultData<T> {
     private T data;
     private long timestamp;
 
-
     public ResultData (){
         this.timestamp = System.currentTimeMillis();
     }
 
+
+    /**
+     * 成功响应
+     * @param data
+     * @return
+     * @param <T>
+     */
     public static <T> ResultData<T> success(T data) {
         ResultData<T> resultData = new ResultData<>();
         resultData.setCode(ReturnCodeEnum.RC200.getCode());
@@ -30,11 +37,32 @@ public class ResultData<T> {
         return resultData;
     }
 
+
+    /**
+     *
+     * @param code 后端处理失败的状态吗
+     * @param message 处理失败的信息
+     * @return data 为空的一个统一封装响应对象
+     * @param <T>
+     */
     public static <T> ResultData<T> fail(String code, String message) {
         ResultData<T> resultData = new ResultData<>();
         resultData.setCode(code);
         resultData.setMessage(message);
+        return resultData;
+    }
 
+
+    /**
+     * 相应后端处理失败的情况
+     * @param returnCodeEnum 枚举类型
+     * @return data 为空的一个统一封装响应对象
+     * @param <T>
+     */
+    public static <T> ResultData<T> fail(ReturnCodeEnum returnCodeEnum) {
+        ResultData<T> resultData = new ResultData<>();
+        resultData.setCode(returnCodeEnum.getCode());
+        resultData.setMessage(returnCodeEnum.getMessage());
         return resultData;
     }
 
