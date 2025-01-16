0669d1e68941dbdea681d613b12a70e3dd8ca279
JieCHERY <9594523wrj@gmail.com>
2025-01-16 15:26:48 +0800

    springcloud

--- b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/entities/PayDTO.java
+++ b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/entities/PayDTO.java
@@ -11,6 +11,9 @@ import java.math.BigDecimal;
  * 给前端交互的数据传输对象
  * 只能暴露 DTO，不能暴露表结构
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PayDTO implements Serializable
 {
     private Integer id;
@@ -23,66 +26,5 @@ public class PayDTO implements Serializable
     //交易金额
     private BigDecimal amount;
 
-    public PayDTO() {
-    }
-
-    public PayDTO(Integer id, Integer userId, String orderNo, String payNo, BigDecimal amount) {
-        this.id = id;
-        this.userId = userId;
-        this.orderNo = orderNo;
-        this.payNo = payNo;
-        this.amount = amount;
-    }
-
-    @Override
-    public String toString() {
-        return "PayDTO{" +
-                "id=" + id +
-                ", payNo='" + payNo + '\'' +
-                ", orderNo='" + orderNo + '\'' +
-                ", userId=" + userId +
-                ", amount=" + amount +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getOrderNo() {
-        return orderNo;
-    }
-
-    public void setOrderNo(String orderNo) {
-        this.orderNo = orderNo;
-    }
-
-    public String getPayNo() {
-        return payNo;
-    }
-
-    public void setPayNo(String payNo) {
-        this.payNo = payNo;
-    }
 }
 
