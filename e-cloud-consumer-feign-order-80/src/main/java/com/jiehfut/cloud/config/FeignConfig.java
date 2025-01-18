a8a17c365cd0e88d09bea611c5020852c930ba28
JieCHERY <9594523wrj@gmail.com>
2025-01-18 13:07:03 +0800

    springcloud

--- e-cloud-consumer-feign-order-80/src/main/java/com/jiehfut/cloud/config/FeignConfig.java
+++ e-cloud-consumer-feign-order-80/src/main/java/com/jiehfut/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.jiehfut.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,4 +25,55 @@ public class FeignConfig {
         // 最大请求次数为 3(1+2)，初始间隔时间为 100ms，即100ms以后启动这个配置，重试间最大间隔时间为 1s
         return new Retryer.Default(100,1,3);
     }
+
+    /**
+     * 对请求和响应进行GZIP压缩
+     * Spring Cloud OpenFeign支持对请求和响应进行 GZIP压缩，以减少通信过程中的性能损耗。
+     * 通过下面的两个参数设置，就能开启请求与相应的压缩功能：
+     * spring.cloud.openfeign.compression.request.enabled=true
+     * spring.cloud.openfeign.compression.response.enabled=true
+     *
+     * 细粒度化设置
+     * 对请求压缩做一些更细致的设置，比如下面的配置内容指定压缩的请求数据类型并设置了请求压缩的大小下限，
+     * 只有超过这个大小的请求才会进行压缩：
+     * spring.cloud.openfeign.compression.request.enabled=true
+     * spring.cloud.openfeign.compression.request.mime-types=text/xml,application/xml,application/json #触发压缩数据类型
+     * spring.cloud.openfeign.compression.request.min-request-size=2048 #最小触发压缩的大小
+     *
+     */
+
+
+    /**
+     * Feign 提供了日志打印功能，我们可以通过配置来调整日志级别，
+     * 从而了解 Feign 中 Http 请求的细节，
+     * 说白了就是对 Feign接口的调用情况进行监控和输出
+     * public enum Level {
+     *     NONE, - No logging.
+     *     BASIC, - Log only the request method and URL and the response status code and execution time.
+     *     HEADERS, - Log the basic information along with request and response headers.
+     *     FULL - Log the headers, body, and metadata for both requests and responses.
+     * }
+     *
+     * 1.配置日志 bean 组件
+     * 2.在 .yml 中配置开启 feign 日志的客户端
+     * 3.后台日志查看
+     *   （1）触发请求响应的压缩功能：
+     *     ---> GET http://cloud-payment-service/pay/get/6 HTTP/1.1
+     *     Accept-Encoding: gzip
+     *     Accept-Encoding: deflate
+     *     ---> END HTTP (0-byte body)
+     *     <--- ERROR SocketTimeoutException: Read timed out (4254ms
+     *     ava.net.SocketTimeoutException: Read timed out
+     *   （2）看到三次请求，openfeign 的重试请求
+     * 5.
+     */
+
+    // 配置日志 bean 组件
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
+
+
+
