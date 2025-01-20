3277ccc0bdbcaab81665215e1037373369b5d0d9
JieCHERY <9594523wrj@gmail.com>
2025-01-20 15:47:55 +0800

    springcloud

---
+++ e-cloud-consumer-feign-order-80/src/main/java/com/jiehfut/cloud/controller/OrderMicrometerController.java
@@ -0,0 +1,29 @@
+package com.jiehfut.cloud.controller;
+
+import com.jiehfut.cloud.apis.PayFeignApi;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 消费者去调用 8001 微服务的链路监控
+ */
+@RestController
+@Slf4j
+public class OrderMicrometerController
+{
+    @Resource
+    private PayFeignApi payFeignApi;
+
+    @GetMapping(value = "/feign/micrometer/{id}")
+    public String myMicrometer(@PathVariable("id") Integer id)
+    {
+        return payFeignApi.myMicrometer(id);
+    }
+}
+
+
+
