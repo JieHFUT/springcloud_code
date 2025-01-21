dc2abf2a5a64762ba85c53a429b99692a60396c2
JieCHERY <9594523wrj@gmail.com>
2025-01-21 17:11:21 +0800

    springcloud

--- b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/controller/PayGateWayController.java
+++ b-cloud-provider-payment-8001/src/main/java/com/jiehfut/cloud/controller/PayGateWayController.java
@@ -1,14 +1,18 @@
 package com.jiehfut.cloud.controller;
 
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.jiehfut.cloud.entities.Pay;
 import com.jiehfut.cloud.resp.ResultData;
 import com.jiehfut.cloud.service.PayService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
 
 
 /**
@@ -29,5 +33,30 @@ public class PayGateWayController {
     @GetMapping(value = "/pay/gateway/info")
     public ResultData<String> getGatewayInfo() {
         return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+
+
+    /**
+     * 测试在使用网关的过程中，对请求转发到控制器方法的前后的处理
+     * 显示请求头中的参数
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request)
+    {
+        String result = "";
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements())
+        {
+            String headName = headers.nextElement();
+            String headValue = request.getHeader(headName);
+            System.out.println("请求头名: " + headName +"\t\t\t"+"请求头值: " + headValue);
+            if(headName.equalsIgnoreCase("X-Request-atguigu1")
+                    || headName.equalsIgnoreCase("X-Request-atguigu2")) {
+                result = result+headName + "\t " + headValue +" ";
+            }
+        }
+        return ResultData.success("getGatewayFilter 过滤器 test： "+result+" \t "+ DateUtil.now());
     }
 }
