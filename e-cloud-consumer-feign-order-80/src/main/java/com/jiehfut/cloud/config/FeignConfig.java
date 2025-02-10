c7f27eb81cab452a7250526c9895491ae1a1d735
JieCHERY <9594523wrj@gmail.com>
2025-02-10 18:13:29 +0800

    springcloud

--- e-cloud-consumer-feign-order-80/src/main/java/com/jiehfut/cloud/config/FeignConfig.java
+++ e-cloud-consumer-feign-order-80/src/main/java/com/jiehfut/cloud/config/FeignConfig.java
@@ -67,7 +67,6 @@ public class FeignConfig {
      *     <--- ERROR SocketTimeoutException: Read timed out (4254ms)
      *     ava.net.SocketTimeoutException: Read timed out
      *   （2）看到三次请求，openfeign 的重试请求
-     *
      */
 
 
